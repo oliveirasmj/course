5af007a2481aec5e32230292d883c6d29e49ba3c
Miguel Oliveira <oliveirasmj@gmail.com>
2021-07-16 15:59:11 +0100

    User entity and resource

---
+++ src/main/java/com/example/course/entities/User.java
@@ -0,0 +1,91 @@
+package com.example.course.entities;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String nome;
+	private int nif;
+	private String morada;
+	private int telefone;
+	
+	public User() {
+	}
+
+	public User(Long id, String nome, int nif, String morada, int telefone) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.nif = nif;
+		this.morada = morada;
+		this.telefone = telefone;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getNif() {
+		return nif;
+	}
+
+	public void setNif(int nif) {
+		this.nif = nif;
+	}
+
+	public String getMorada() {
+		return morada;
+	}
+
+	public void setMorada(String morada) {
+		this.morada = morada;
+	}
+
+	public int getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(int telefone) {
+		this.telefone = telefone;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+}
